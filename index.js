e25dc4e7793c3c846c3b87fcf7c87f2323952ee9
mutotu <muttalip.aydemir@gmail.com>
2022-03-28 21:43:19 +0100

    finished workd

--- index.js
+++ index.js
@@ -1,29 +1,68 @@
 const search = document.querySelector(".searchArtist");
 const searchButton = document.querySelector(".search-button");
 const sectionList = document.querySelector(".section-list");
+const h3 = document.querySelector("h3");
+let searched = "";
+const error = document.querySelector(".error");
+const loading = document.querySelector(".load");
+
+/////
 async function searchArtist(artist) {
   const response = fetch(`https://itunes.apple.com/search?term=${artist}`)
     .then((res) => res.json())
     .then((json) => {
       console.log(json);
       renderCardList(json);
+      loader();
     });
 }
 
+function loader() {
+  loading.classList.add("loading");
+}
+function unloader() {
+  loading.classList.remove("loading");
+}
+
+function checkInput(value) {
+  if (value.length > 3) return true;
+}
+////change search bar
+//// modify the eventlisteners and error message
+search.addEventListener("keypress", (e) => {
+  if (e.keyCode === 13) {
+    // loader();
+    searched = search.value;
+    if (checkInput(search.value)) {
+      searchArtist(search.value);
+      error.innerHTML = null;
+      search.value = null;
+    } else {
+      console.log("wrongs");
+      error.innerHTML = "Please enter valid entry";
+    }
+  }
+});
 searchButton.addEventListener("click", () => {
-  searchArtist(search.value);
+  if (checkInput(search.value)) {
+    // loader();
+    searched = search.value;
+    searchArtist(search.value);
+    error.innerHTML = null;
+    search.value = null;
+  } else {
+    console.log("wrongs");
+    error.innerHTML = "Please enter valid entry";
+  }
 });
 
 function generateCard(artist) {
   return `<div class='artist-card' id='${artist.artistId}'>
-  <img src='${artist.artworkUrl100}' />
-    <p>Name:'${artist.artistName}' </p>
-    <p>Track name:'${artist.trackName}'</p>
-    <audio controls>
-  <source src="${artist.previewUrl}" >
- 
-  Your browser does not support the audio element.
-</audio>
+  
+  <img src=${artist.artworkUrl100} />
+    <p>${artist.artistName} </p>
+    <p>${artist.trackName}</p>    
+  
   </div>`;
 }
 
@@ -36,4 +75,5 @@ function renderCardList(artists) {
 
 function insertIntoHtml(data) {
   sectionList.insertAdjacentHTML("afterbegin", data);
+  h3.innerHTML = `${data.length} results for "${searched}"`;
 }
